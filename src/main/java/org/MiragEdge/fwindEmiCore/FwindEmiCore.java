ec7ff76ed9e32b98fbda5b15ed13cb209c5cd357
HFwindEmi <2011857087@qq.com>
2025-05-23 20:54:50 +0800

    FirstPull

---
+++ src/main/java/org/MiragEdge/fwindEmiCore/FwindEmiCore.java
@@ -0,0 +1,17 @@
+package org.MiragEdge.fwindEmiCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FwindEmiCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
